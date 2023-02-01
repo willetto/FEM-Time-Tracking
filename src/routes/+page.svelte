9d983ff1f3bfd48e3552b2e2a4d3c0d430a33b86
Trey Willetto <trey.willetto@gmail.com>
2023-02-01 12:06:25 -0700

    Init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Timecard from './Timecard.svelte';
+	import WorkIcon from '$lib/imgs/icon-work.svg';
+	import PlayIcon from '$lib/imgs/icon-play.svg';
+	import StudyIcon from '$lib/imgs/icon-study.svg';
+	import ExerciseIcon from '$lib/imgs/icon-exercise.svg';
+
+	import Data_file from '$lib/data.json';
+	const work_data = Data_file[0];
+	const play_data = Data_file[1];
+	const study_data = Data_file[2];
+	const exercise_data = Data_file[3];
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<Timecard bgclass="work" cardData={work_data}>
+	<img src={WorkIcon} alt="" slot="icon" />
+</Timecard>
+
+<Timecard bgclass="play" cardData={play_data}>
+	<img src={PlayIcon} alt="" slot="icon" />
+</Timecard>
+
+<Timecard bgclass="study" cardData={study_data}>
+	<img src={StudyIcon} alt="" slot="icon" />
+</Timecard>
+
+<Timecard bgclass="exercise" cardData={exercise_data}>
+	<img src={ExerciseIcon} alt="" slot="icon" />
+</Timecard>
+
+<style lang="scss">
+	h1 {
+		color: $blue-400;
+	}
+</style>
---
+++ src/routes/Timecard.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	type cardObject = {
+		title: string;
+		timeframes: {
+			daily: {
+				current: number;
+				previous: number;
+			};
+			weekly: {
+				current: number;
+				previous: number;
+			};
+			monthly: {
+				current: number;
+				previous: number;
+			};
+		};
+	};
+	export let bgclass: String;
+	export let cardData: cardObject;
+</script>
+
+<div class="card {bgclass}">
+	<div class="card-color">
+		<slot name="icon" />
+	</div>
+	<div class="card-info">
+		<h1>{cardData.title}</h1>
+	</div>
+</div>
+
+<style lang="scss">
+	.card {
+		&-color {
+			padding: 2rem;
+			background-color: var(--bg-color);
+		}
+	}
+	.work {
+		--bg-color: #{$work-color};
+	}
+	.play {
+		--bg-color: #{$play-color};
+	}
+	.study {
+		--bg-color: #{$study-color};
+	}
+	.exercise {
+		--bg-color: #{$exercise-color};
+	}
+	.social {
+		--bg-color: #{$social-color};
+	}
+	.selfcare {
+		--bg-color: #{$selfcare-color};
+	}
+</style>
